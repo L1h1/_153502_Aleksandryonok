4226c0965c680683af1c84a78df964b33c19907f
L1h1 <i.aleksandrionok@gmail.com>
2022-03-16 17:29:19 +0300

    Initial commit

---
+++ _153502_Aleksandryonok/Program.cs
@@ -0,0 +1,13 @@
+﻿namespace _153502_Aleksandryonok;
+
+public static class MainModule
+{
+    public static void Main(string[] args)
+    {
+        Console.WriteLine("enter 2 int values");
+        int a = Convert.ToInt32(Console.ReadLine());
+        int b = Convert.ToInt32(Console.ReadLine());
+        double result = (double)a / b;
+        Console.WriteLine($"Result: {result}");
+    }
+}
