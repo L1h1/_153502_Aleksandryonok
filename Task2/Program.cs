9ad7079de63f9ddd127813a498acc46705f42267
L1h1 <i.aleksandrionok@gmail.com>
2022-04-04 19:00:47 +0300

    x

--- Task2/Program.cs
+++
@@ -1,9 +0,0 @@
-﻿using Task2.Services;
-
-Console.WriteLine("Enter integer a,b,z");
-
-int a=Convert.ToInt32(Console.ReadLine());
-int b=Convert.ToInt32(Console.ReadLine());
-int z=Convert.ToInt32(Console.ReadLine());
-
-Console.WriteLine($"Function result:{Calculator.ProcessFunction(a,b,z)}");
