c5b058f1e0701afcebc93323cfbdbfa8641efae1
L1h1 <i.aleksandrionok@gmail.com>
2022-05-04 17:42:38 +0300

    x

--- TEST/UnitTest1.cs
+++ TEST/UnitTest1.cs
@@ -11,67 +11,30 @@ public class Tests
     }
 
     [Test]
-    public void ClientTestSummary()
+    public void printNameNoOverload()
     {
-        Client cl = new Client("a", "b", "c");
-        cl.ApplyTransaction(new Ticket(100,Station.Rate.First));
-        cl.ApplyTransaction(new Ticket(45,Station.Rate.Second));
-        cl.ApplyTransaction(new Ticket(37.1,Station.Rate.Third));
-        Assert.AreEqual(cl.GetTransactionSummary(),182.1,0.000001);
+        School sc = new School("abauba", 15, 15);
+        Assert.AreEqual(sc.PrintName(),"учреждение образования abauba");
     }
     [Test]
-    public void ClientTestOnRate()
+    public void printNameOverload()
     {
-        Client cl = new Client("a", "b", "c");
-        cl.ApplyTransaction(new Ticket(100,Station.Rate.First));
-        cl.ApplyTransaction(new Ticket(45,Station.Rate.Second));
-        Assert.False(cl.IsOnRate(Station.Rate.Third));
-        Assert.True(cl.IsOnRate(Station.Rate.First));
-        Assert.True(cl.IsOnRate(Station.Rate.Second));
+        University sc = new University("abauba", 15);
+        Assert.AreEqual(sc.PrintName(),"Добро пожаловать в университет abauba!");
     }
     [Test]
-    public void ClientTestData()
+    public void Dynamics()
     {
-        Client cl = new Client("a", "b", "c");
-        Assert.AreEqual(cl.GetClientData(),"a\tb\tc");
+        University sc = new University("abauba", 15);
+        sc.FailCount = 100;
+        Assert.AreEqual(sc.GetFailDynamic(),85);
     }
     [Test]
-    public void TicketTestData()
+    public void PrintInf()
     {
-        Ticket ticket = new Ticket(273.324,Station.Rate.Second);
-        Assert.AreEqual(ticket.GetData(),"273,324 Second");
+        University sc = new University("abauba", 15);
+        sc.FailCount = 100;
+        Assert.AreEqual(sc.PrintInfo(),"число несдавших лабы: 100");
     }
-    [Test]
-    public void StationListings()
-    {
-        Station saratov = new Station("Saratov",10.5,90.5,99.9);
-        saratov.RegisterClient("John","Wick","sbkwegiu4rt4w83");
-        saratov.RegisterClient("Victor","Heim","es565453wtgxw43");
-        saratov.RegisterClient("David","King","zwekii342i2rk2g");
-        saratov.RegisterClient("William","Owerbek","vcxmnn3gjmjkw21");
-        saratov.SetActiveClient(0);
-        saratov.ProcessTransaction(Station.Rate.First);
-        saratov.ProcessTransaction(Station.Rate.Third);
-        saratov.SetActiveClient(1);
-        saratov.ProcessTransaction(Station.Rate.First);
-        saratov.ProcessTransaction(Station.Rate.Second);
-        saratov.SetActiveClient(2);
-        saratov.ProcessTransaction(Station.Rate.Second);
-        saratov.ProcessTransaction(Station.Rate.Third);
-        saratov.SetActiveClient(3);
-        saratov.ProcessTransaction(Station.Rate.First);
-        saratov.ProcessTransaction(Station.Rate.Second);
-        saratov.ProcessTransaction(Station.Rate.Third);
-        Assert.AreEqual(saratov.ShowClients(),"###################################Client_List###################################\n" +
-                                              "[0] John\tWick\tsbkwegiu4rt4w83\n" +
-                                              "[1] Victor\tHeim\tes565453wtgxw43\n" +
-                                              "[2] David\tKing\tzwekii342i2rk2g\n" +
-                                              "[3] William\tOwerbek\tvcxmnn3gjmjkw21\n" +
-                                              "#################################################################################");
-        Assert.AreEqual(saratov.ShowRateTransactions(Station.Rate.First),"#################################First########################################\n" +
-                                                                          "[0] John\tWick\tsbkwegiu4rt4w83\n" +
-                                                                          "[1] Victor\tHeim\tes565453wtgxw43\n" +
-                                                                          "[3] William\tOwerbek\tvcxmnn3gjmjkw21\n" +
-                                                                          "#################################################################################");
-    }
+    
 }
--- Task/Program.cs
+++ Task/Program.cs
@@ -1,36 +1,15 @@
-﻿
-    using Task;
+﻿using Task;
+//demo
+University uni = new University("БГУИР", 13465);
+Console.WriteLine(uni.PrintInfo());
+Console.WriteLine(uni.PrintName());
 
-    Station saratov = new Station("Saratov",10.5,90.5,99.9);
-    //регистрация клиентов по паспартным данным
-    saratov.RegisterClient("John","Wick","sbkwegiu4rt4w83");
-    saratov.RegisterClient("Victor","Heim","es565453wtgxw43");
-    saratov.RegisterClient("David","King","zwekii342i2rk2g");
-    saratov.RegisterClient("William","Owerbek","vcxmnn3gjmjkw21");
-    Console.WriteLine(saratov.ShowClients());
-    saratov.SetActiveClient(0);
-    saratov.ProcessTransaction(Station.Rate.First);
-    saratov.ProcessTransaction(Station.Rate.Third);
-    saratov.SetActiveClient(1);
-    saratov.ProcessTransaction(Station.Rate.First);
-    saratov.ProcessTransaction(Station.Rate.Second);
-    saratov.SetActiveClient(2);
-    saratov.ProcessTransaction(Station.Rate.Second);
-    saratov.ProcessTransaction(Station.Rate.Third);
-    saratov.SetActiveClient(3);
-    saratov.ProcessTransaction(Station.Rate.First);
-    saratov.ProcessTransaction(Station.Rate.Second);
-    saratov.ProcessTransaction(Station.Rate.Third);
-    
-    saratov.SetActiveClient(0);
-    Console.WriteLine(saratov.GetClientSummary());
-    saratov.SetActiveClient(1);
-    Console.WriteLine(saratov.GetClientSummary());
-    saratov.SetActiveClient(2);
-    Console.WriteLine(saratov.GetClientSummary());
-    saratov.SetActiveClient(3);
-    Console.WriteLine(saratov.GetClientSummary());
-    
-    Console.WriteLine(saratov.ShowRateTransactions(Station.Rate.First));
-    Console.WriteLine(saratov.ShowRateTransactions(Station.Rate.Second));
-    Console.WriteLine(saratov.ShowRateTransactions(Station.Rate.Third));
+uni.FailCount=14000;
+Console.WriteLine("Динамика несдавших лабы "+uni.GetFailDynamic());
+uni.ShowRepairStatus();//не скрыт
+
+Console.WriteLine("-----------------");
+School sch = new School("GYMN2",40,1400);
+Console.WriteLine(sch.PrintInfo());
+Console.WriteLine(sch.PrintName());
+sch.ShowRepairStatus();//скрыт здесь
